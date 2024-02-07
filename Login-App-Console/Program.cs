287dbc7b63c90fc926117eb5821496eff9e60144
Levin Baenninger <levin.baenninger@buhlergroup.com>
2024-02-07 10:05:57 +0100

    Projektdateien hinzufügen.

---
+++ Login-App-Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Login_App_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
